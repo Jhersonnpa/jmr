210ad3e5fcdb7319f3aae8c5bf444b6ce9f162dc
Marcmuu <morellgumi@gmail.com>
2024-02-06 15:25:44 +0100

    script de layout cambiado a archivo de scripts

--- src/components/Scripts.jsx
+++ src/components/Scripts.jsx
@@ -123,6 +123,48 @@ const Scripts = () => {
       }
     });
     
+    if (document.startViewTransition) {
+      // Agrega un event listener para el evento 'navigate'
+      window.navigation.addEventListener('navigate', (event) => {
+        const toUrl = new URL(event.destination.url);
+    
+        // Es una página externa? Si es así, lo ignoramos
+        if (location.origin !== toUrl.origin) return;
+    
+        // Si es una navegación en el mismo dominio (origen)
+        event.intercept({
+          async handler() {
+            try {
+              // Cargamos la página de destino utilizando fetch para obtener el HTML
+              const response = await fetch(toUrl.pathname);
+              const text = await response.text();
+              const regexHTML = /<html[^>]*>([\s\S]*?)<\/html>/i;
+    
+              // Extraemos solo el contenido del HTML dentro de la etiqueta body
+              const [, data] = text.match(regexHTML) || [];
+    
+              if (data) {
+                // Utilizamos la API de View Transition
+                document.startViewTransition(() => {
+                  const html = document.getElementById('html');
+    
+                  if (html) {
+                    // Hacemos scroll hacia arriba del todo
+                    html.innerHTML = data;
+                    // document.documentElement.scrollTop = 0;
+                  }
+                });
+              }
+            } catch (error) {
+              console.error('Error al cargar la página:', error);
+            }
+          },
+        });
+      });
+    }
+
+
+
   }, []);
   return <></>;
 };
