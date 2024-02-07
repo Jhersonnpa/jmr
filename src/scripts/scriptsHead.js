ecf47db6b219b90cd5f29b3ba6df638e26784098
Jhersonnpa <jhersonnpa@gmail.com>
2024-02-07 11:58:19 +0100

    Implementación de nuevo del View Transition de Astro

---
+++ src/scripts/scriptsHead.js
@@ -0,0 +1,38 @@
+const theme = (() => {
+  if (typeof localStorage !== "undefined" && localStorage.getItem("theme")) {
+    return localStorage.getItem("theme");
+  }
+  if (window.matchMedia("(prefers-color-scheme: dark)").matches) {
+    return "dark";
+  }
+  return "light";
+})();
+
+if (theme === "light") {
+  document.documentElement.classList.remove("dark");
+} else {
+  document.documentElement.classList.add("dark");
+}
+
+window.localStorage.setItem("theme", theme);
+
+// View Transition
+document.addEventListener("astro:page-load", () => {
+  const theme = (() => {
+    if (typeof localStorage !== "undefined" && localStorage.getItem("theme")) {
+      return localStorage.getItem("theme");
+    }
+    if (window.matchMedia("(prefers-color-scheme: dark)").matches) {
+      return "dark";
+    }
+    return "light";
+  })();
+
+  if (theme === "light") {
+    document.documentElement.classList.remove("dark");
+  } else {
+    document.documentElement.classList.add("dark");
+  }
+
+  window.localStorage.setItem("theme", theme);
+});
