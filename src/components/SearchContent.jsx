7a92efc8b4936307a9c64136b9cbcf2fbaf26cbb
Marcmuu <morellgumi@gmail.com>
2024-02-07 13:23:37 +0100

    top del aside ajustado, search notfound responsive

--- src/components/SearchContent.jsx
+++ src/components/SearchContent.jsx
@@ -153,31 +153,18 @@ const SearchContent = () => {
           ))}
         </div>
       ) : (
-        <div className="w-1/2 rounded-md h-full p-20 mx-auto flex justify-center items-center flex-col gap-4 bg-white/30 dark:bg-white/10 backdrop-blur-xl shadow-2xl">
+        <div className="md:w-3/4 lg:w-1/2 rounded-md h-full p-5 md:pb-10 mx-auto flex justify-center items-center flex-col gap-4 bg-white/30 dark:bg-white/10 backdrop-blur-xl shadow-2xl">
           <div>
-            <img src="/error.gif" alt="error gif" className="w-1/2 object-contain mx-auto"/>
-            {/* <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="icon icon-tabler icon-tabler-device-desktop-x h-24 w-24"
-              width="24"
-              height="24"
-              viewBox="0 0 24 24"
-              strokeWidth="2"
-              stroke="orangered"
-              fill="none"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-            >
-              <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-              <path d="M13 16h-9a1 1 0 0 1 -1 -1v-10a1 1 0 0 1 1 -1h16a1 1 0 0 1 1 1v8" />
-              <path d="M7 20h6.5" />
-              <path d="M9 16v4" />
-              <path d="M22 22l-5 -5" />
-              <path d="M17 22l5 -5" />
-            </svg> */}
+            <img
+              src="/error.gif"
+              alt="error gif"
+              className="w-1/2 object-contain mx-auto"
+            />
           </div>
-          <p className="font-bold dark:text-white text-3xl text-pretty">No se ha encontrado el recurso:</p>
-          <p className="font-semibold text-xl dark:text-white">
+          <p className="font-bold dark:text-white text-sm md:text-lg lg:text-xl text-pretty">
+            No se ha encontrado el recurso:
+          </p>
+          <p className="font-semibold text-sm md:text-lg lg:text-xl dark:text-white">
             ' <span>{$buscador}</span> '
           </p>
         </div>
