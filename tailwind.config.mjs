6b65df4b653d7ea5303b43f7d5311f9edd6d8815
Jhersonnpa <jhersonnpa@gmail.com>
2024-02-12 07:27:23 +0100

    Creación de link github del proyecto en el navbar, ampliación de recursos, un poco de rediseño de CardItem y cambio en los tags de categorias, ahora pasan a ser links

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,9 +1,25 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	darkMode: 'class',
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  darkMode: "class",
+  theme: {
+    extend: {
+      colors: {
+        bunker: {
+          50: "#f6f7f9",
+          100: "#eceef2",
+          200: "#d5dae2",
+          300: "#b0b9c9",
+          400: "#8593ab",
+          500: "#667691",
+          600: "#515f78",
+          700: "#434d61",
+          800: "#3a4252",
+          900: "#343a46",
+          950: "#13151a",
+        },
+      },
+    },
+  },
+  plugins: [],
+};
